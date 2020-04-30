84e2fe303abfb8b10f0e82f16bec7ecb4725e9f4
toshi0383 <t.suzuki326@gmail.com>
2020-04-30 17:30:34 +0900

    [Abceed] fix build

--- abceed/AppDelegate.swift
+++ abceed/AppDelegate.swift
@@ -9,7 +9,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
         self.window = window
-        window.rootViewController = TopCategoryTabBuilder().build()
+        window.rootViewController = TopCategoryTabBuilder().build(window.windowScene!).viewController
         window.makeKeyAndVisible()
 
         return true
